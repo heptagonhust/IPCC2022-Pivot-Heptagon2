993f90af90eb674d1a5614d97e2c0d80ed3ce1bc
Shawlley <wsyy0619@gmail.com>
2022-08-04 14:55:53 +0800

    feat: remove redundent calculation

--- pivot.cc
+++ pivot.cc
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<map>
 #include<sys/time.h>
 
 // Calculate sum of distance while combining different pivots. Complexity : O( n^2 )
@@ -45,7 +46,7 @@ double SumDistance(const int k, const int n, const int dim, double* coord, int* 
     return chebyshevSum;
 }
 
-// Recursive function Combination() : combine pivots and calculate the sum of distance while combining different pivots.
+// Recursive function recursive_combinations() : combine pivots and calculate the sum of distance while combining different pivots.
 // ki  : current depth of the recursion
 // k   : number of pivots
 // n   : number of points
@@ -57,7 +58,7 @@ double SumDistance(const int k, const int n, const int dim, double* coord, int* 
 // maxDisSumPivots : the top M pivots combinations
 // minDistanceSum  : the smallest M distance sum
 // minDisSumPivots : the bottom M pivots combinations
-void Combination(int ki, const int k, const int n, const int dim, const int M, double* coord, int* pivots,
+void recursive_combinations(int ki, const int k, const int n, const int dim, const int M, double* coord, int* pivots,
                  double* maxDistanceSum, int* maxDisSumPivots, double* minDistanceSum, int* minDisSumPivots){
     if(ki==k-1){
         int i;
@@ -113,26 +114,187 @@ void Combination(int ki, const int k, const int n, const int dim, const int M, d
     int i;
     for(i=pivots[ki-1]+1; i<n; i++) {
         pivots[ki] = i;
-        Combination(ki+1, k, n, dim, M, coord, pivots, maxDistanceSum, maxDisSumPivots, minDistanceSum, minDisSumPivots);
+        recursive_combinations(ki+1, k, n, dim, M, coord, pivots, maxDistanceSum, maxDisSumPivots, minDistanceSum, minDisSumPivots);
 
         /** Iteration Log : pivots computed, best pivots, max distance sum, min distance sum pivots, min distance sum
         *** You can delete the logging code. **/
-        if(ki==k-2){
-            int kj;
-            for(kj=0; kj<k; kj++){
-                printf("%d ", pivots[kj]);
-            }
-            putchar('\t');
-            for(kj=0; kj<k; kj++){
-                printf("%d ", maxDisSumPivots[kj]);
-            }
-            printf("%lf\t", maxDistanceSum[0]);
-            for(kj=0; kj<k; kj++){
-                printf("%d ", minDisSumPivots[kj]);
-            }
-            printf("%lf\n", minDistanceSum[0]);
+        // if(ki==k-2){
+        //     int kj;
+        //     for(kj=0; kj<k; kj++){
+        //         printf("%d ", pivots[kj]);
+        //     }
+        //     putchar('\t');
+        //     for(kj=0; kj<k; kj++){
+        //         printf("%d ", maxDisSumPivots[kj]);
+        //     }
+        //     printf("%lf\t", maxDistanceSum[0]);
+        //     for(kj=0; kj<k; kj++){
+        //         printf("%d ", minDisSumPivots[kj]);
+        //     }
+        //     printf("%lf\n", minDistanceSum[0]);
+        // }
+    }
+}
+
+
+int next_comb(int *arr, int n, int k) {
+    int pos = k-1;
+    while(pos >= 0 && n - arr[pos] == k - pos) {
+        pos = pos - 1;
+    }
+    if(pos == -1) {
+        return -1;
+    } 
+    int ret = pos;
+    arr[pos] ++;
+    for(int i = pos + 1; i < k; i++) {
+        arr[i] = arr[i-1] + 1;
+    }
+    return ret;
+}
+
+double distance(double *coord, int ndims, int x, int y) {
+    double dist = .0;
+    for(int i = 0; i < ndims; i++) {
+        dist += (coord[ndims*x + i] - coord[ndims*y + i]) * (coord[ndims*x + i] - coord[ndims*y + i]);
+    }
+    return sqrt(dist);
+}
+
+double calc_value(const int prev, const int npoints, const int npivots, const int ndims, int *pivots, double *coord, double *rebuilt_coord, double *mx) {
+    // Part 1. Rebuild Coordintate System
+    for(int k = prev; k < npivots; k++) {
+        int p = pivots[k];
+        for(int i = 0; i < npoints; i++) {
+            rebuilt_coord[k*npoints + i] = distance(coord, ndims, p, i);
         }
     }
+
+    // Part 2. Evaluate System Value
+
+    // Part 2.1. Calculate Chebyshev Distance
+    for(int k = prev; k < npivots; k++) {
+        for(int i = 0; i < npoints; i++) {
+            for(int j = i+1; j < npoints; j++) {
+                double chebyshev_dim_dist = fabs(rebuilt_coord[k*npoints + i] - rebuilt_coord[k*npoints + j]);
+                if(k > 0 && chebyshev_dim_dist < mx[(k-1)*npoints*npoints + i*npoints + j]) {
+                    chebyshev_dim_dist = mx[(k-1)*npoints*npoints + i*npoints + j];
+                }
+                mx[k*npoints*npoints + i*npoints + j] = chebyshev_dim_dist;
+            }
+        }
+    }
+    
+    // Part 2.2. Calculate Sum of Chebyshev Distance between Each Pair
+
+    double *chebyshev_dist = &mx[(npivots-1)*npoints*npoints]; 
+    double chebyshev_dist_sum = .0;
+    for(int i = 0; i < npoints; i++) {
+        for(int j = i+1; j < npoints; j++) {
+            chebyshev_dist_sum += chebyshev_dist[i*npoints+j];
+        }
+    }
+
+    // Calculate Half of All Pairs, Then Double
+    return chebyshev_dist_sum * 2;
+}
+
+// maxDisSum, minDisSum, maxDisSumPivots, minDisSumPivots
+void combinations(const int npoints, const int npivots, const int ndims, const int M, double *coord, int *pivots, 
+                  double* maxDistanceSum, int* maxDisSumPivots, double* minDistanceSum, int* minDisSumPivots) {
+
+    double *rebuilt_coord = (double*) malloc(sizeof(double) * npivots * npoints);
+    double *mx = (double*) malloc(sizeof(double) * npivots * npoints * npoints);
+    int *maxTmpPivots = (int *) malloc(sizeof(int) * M * npivots);
+    int *minTmpPivots = (int *) malloc(sizeof(int) * M * npivots);
+    std::map<double, int> mx_mp{};
+    std::map<double, int> mn_mp{};
+    for(int i = 0; i < npivots; i++) {
+        pivots[i] = i;
+    }
+    int prev = 0;
+    while(prev != -1) {
+        double value = calc_value(prev, npoints, npivots, ndims, pivots, coord, rebuilt_coord, mx);
+
+        // Part 3. Get Top M and Bottom M
+
+        // Part 3.1. Top M cases
+        int size = (int)mx_mp.size();
+        if(size < M) {
+            int idx = (int)mx_mp.size();
+            mx_mp.insert(std::map<double, int>::value_type(value, idx));
+            for(int i = 0; i < npivots; i++) {
+                maxTmpPivots[idx*npivots + i] = pivots[i];
+            }
+        } else {
+            auto iter = mx_mp.begin();
+            if(iter->first < value) {
+                int idx = iter->second;
+                mx_mp.erase(iter);
+                mx_mp.insert(std::map<double, int>::value_type(value, idx));
+                for(int i = 0; i < npivots; i++) {
+                    maxTmpPivots[idx*npivots + i] = pivots[i];
+                }
+            }
+        }
+
+        // Part 3.2. Bottom M cases
+        size = (int)mn_mp.size();
+        if(size < M) {
+            int idx = (int)mn_mp.size();
+            mn_mp.insert(std::map<double, int>::value_type(value, idx));
+            for(int i = 0; i < npivots; i++) {
+                minTmpPivots[idx*npivots + i] = pivots[i];
+            }
+        } else {
+            auto iter = mn_mp.end();
+            iter --;
+            if(iter->first > value) {
+                int idx = iter->second;
+                mn_mp.erase(iter);
+                mn_mp.insert(std::map<double, int>::value_type(value, idx));
+                for(int i = 0; i < npivots; i++) {
+                    minTmpPivots[idx*npivots + i] = pivots[i];
+                }
+            }
+        }
+
+        prev = next_comb(pivots, npoints, npivots);
+    }
+
+    // Part 4. Sort Answer_pivots arrays
+    
+    int idx = 0;
+    for(auto iter = mx_mp.rbegin(); iter != mx_mp.rend(); iter++) {
+        int tmp_idx = iter->second;
+        for(int i = 0; i < npivots; i++) {
+            maxDisSumPivots[idx*npivots + i] = maxTmpPivots[tmp_idx*npivots + i]; 
+        }
+        maxDistanceSum[idx] = iter->first;
+        idx++;        
+    }
+
+    idx = 0;
+    for(auto iter = mn_mp.begin(); iter != mn_mp.end(); iter++) {
+        int tmp_idx = iter->second;
+        for(int i = 0; i < npivots; i++) {
+            minDisSumPivots[idx*npivots + i] = minTmpPivots[tmp_idx*npivots + i]; 
+        }
+        minDistanceSum[idx] = iter->first;
+        idx++;        
+    }
+
+    free(maxTmpPivots);
+    free(minTmpPivots);
+    free(rebuilt_coord);
+    free(mx);
+}
+
+void Combination(int ki, const int k, const int n, const int dim, const int M, double* coord, int* pivots,
+                 double* maxDistanceSum, int* maxDisSumPivots, double* minDistanceSum, int* minDisSumPivots) {
+    
+    combinations(n, k, dim, M, coord, pivots, maxDistanceSum, maxDisSumPivots, minDistanceSum, minDisSumPivots);
+    
 }
 
 int main(int argc, char* argv[]){
